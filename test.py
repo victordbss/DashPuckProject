a2fb35aa427c468fd4224bb7efb94d40fbec21bb
victordbss <primevictordubois@gmail.com>
2025-11-06 21:29:26 +0100

    ajout

--- test.py
+++ test.py
@@ -1,6 +1,25 @@
 from setup import *
-import random
+import time
 
+play_music("power_up")
+tour = 0
+tour_pass = False
+servo2.set_angle(90)
 while True:
-    if button_a.is_pressed():
-        play_music("jump_up")
+    motor1.set_speed(70)
+    if sensor26.get_value() <= 25000:
+        if not tour_pass:
+            tour += 1
+        if tour >= 2:
+            servo2.set_angle(180)
+            motor1.set_speed(0)
+            time.sleep(3)
+            servo2.set_angle(90)
+            time.sleep(1)
+            tour = 0
+        tour_pass = True
+    else:
+        tour_pass = False
+    
+    time.sleep(0.01)
+    print(sensor26.get_value())
