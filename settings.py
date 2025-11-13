6ede39c173d5f9bf9555b523bddcade8f4ee13a7
victordbss <primevictordubois@gmail.com>
2025-11-13 15:50:31 +0100

    Ajout des premières étapes principales

--- settings.py
+++ settings.py
@@ -2,8 +2,13 @@
 
 # Motor 1 => Moteur qui fait tourner le bras principal
 # Sensor 26 => Capteur qui détecte le passage du bras robotique
-
+# Servo 2 => Servo moteur qui bloque le bras (petit)
 
 
 MOTOR_SPEED_TO_SENSOR = 50 # Vitesse du moteur qui tient le bras jusqu'au capteur (étape1)
 BW_SENSOR_CHANGE_VALUE = 20000 # Valeur du capteur blanc et noir quand il détecte le passage du bras robotique
+BUTTON_SENSOR_VALUE = 10000 # Valeur minimale qui ne détecte pas un appui sur le bouton
+TIME_FROM_SENSOR_TO_SERVO = 0.03 # Temps que met le bras robotique à passer du capteur bw au servo qui le stoppe
+
+STOP_SERVO_START_ANGLE = 0 # Angle du servo moteur bloqueur pour lequel il ne stoppe pas le chemin du bras robotique
+STOP_SERVO_END_ANGLE = 90 # Angle du servo moteur bloqueur pour lequel il stoppe le bras robotique
--- test.py
+++ test.py
@@ -2,12 +2,46 @@ from setup import *
 import settings as stg
 import time
 
-play_music("power_up")
 
 
 #Etape qui ammène le bras jusqu'au capteur 
-def step1():
+def step1() -> bool:
     motor1.set_speed(stg.MOTOR_SPEED_TO_SENSOR)
     while sensor26.get_value() >= stg.BW_SENSOR_CHANGE_VALUE:
+        print(sensor26.get_value())
         time.sleep(0.01)
     motor1.set_speed(0)
+    return True
+
+def step2() -> bool:
+    motor_speed = 50
+    motor1.set_speed(motor_speed)
+    time.sleep(1)
+    
+    while motor_speed < 100:
+        motor1.set_speed(motor_speed)
+        motor_speed += 1
+        time.sleep(0.01)
+        print(motor_speed)
+
+    return True
+
+def step3() -> bool:
+    while sensor26.get_value() >= stg.BW_SENSOR_CHANGE_VALUE:
+        time.sleep(0.01)
+    servo2.set_angle(stg.STOP_SERVO_END_ANGLE)
+    time.sleep(stg.TIME_FROM_SENSOR_TO_SERVO)
+    motor1.set_speed(0)
+    time.sleep(1)
+    servo2.set_angle(stg.STOP_SERVO_START_ANGLE)
+
+while True:
+    if sensor27.get_value() <= stg.BUTTON_SENSOR_VALUE:
+        if step1():
+            print("done")
+            time.sleep(1)
+            if step2():
+                print("done")
+                step3()
+
+    time.sleep(0.5)
