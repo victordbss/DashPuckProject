122f18bbf1de9e9a12fdf88ffecedf4ee675e4ad
victordbss <primevictordubois@gmail.com>
2025-11-13 14:22:27 +0100

    Ajout du fichier settings.py et d'un exemple de l'étape 1

---
+++ settings.py
@@ -0,0 +1,9 @@
+# Code qui gère tout les paramètres de la machine (variable) et le cablage
+
+# Motor 1 => Moteur qui fait tourner le bras principal
+# Sensor 26 => Capteur qui détecte le passage du bras robotique
+
+
+
+MOTOR_SPEED_TO_SENSOR = 50 # Vitesse du moteur qui tient le bras jusqu'au capteur (étape1)
+BW_SENSOR_CHANGE_VALUE = 20000 # Valeur du capteur blanc et noir quand il détecte le passage du bras robotique
--- test.py
+++ test.py
@@ -1,25 +1,13 @@
 from setup import *
+import settings as stg
 import time
 
 play_music("power_up")
-tour = 0
-tour_pass = False
-servo2.set_angle(90)
-while True:
-    motor1.set_speed(70)
-    if sensor26.get_value() <= 25000:
-        if not tour_pass:
-            tour += 1
-        if tour >= 2:
-            servo2.set_angle(180)
-            motor1.set_speed(0)
-            time.sleep(3)
-            servo2.set_angle(90)
-            time.sleep(1)
-            tour = 0
-        tour_pass = True
-    else:
-        tour_pass = False
-    
-    time.sleep(0.01)
-    print(sensor26.get_value())
+
+
+#Etape qui ammène le bras jusqu'au capteur 
+def step1():
+    motor1.set_speed(stg.MOTOR_SPEED_TO_SENSOR)
+    while sensor26.get_value() >= stg.BW_SENSOR_CHANGE_VALUE:
+        time.sleep(0.01)
+    motor1.set_speed(0)
